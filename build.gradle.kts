19dcc011230615b823c929ff7f799a55d58ec780
Yonatan Karp-Rudin <yonvata@gmail.com>
2024-09-21 23:01:14 +0200

    Init Project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "com.yonatankarp.seamcarving.AppKt"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/yonatankarp/seamcarving/AppTest.kt
@@ -0,0 +1,12 @@
+package com.yonatankarp.seamcarving
+
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+class AppTest {
+    @Test
+    fun appHasAGreeting() {
+        val classUnderTest = App()
+        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    }
+}
